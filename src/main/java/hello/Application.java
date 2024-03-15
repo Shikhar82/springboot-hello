b9111f4460994e1ba4d7e67e50d8f9b29f311646
dereddimurali <57778736+dereddimurali@users.noreply.github.com>
2024-03-15 14:28:39 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {   
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
